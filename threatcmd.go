30f1717de15835e4697ac604a9e067d85c77bc70
Jon Heise <j.heise@gmail.com>
2016-03-01 23:57:57 -0800

    updated to use new library revision and file hash

--- threatcmd.go
+++ threatcmd.go
@@ -27,7 +27,7 @@ func main(){
         fmt.Printf("Permalink: %s\n", ip_data.Permalink)
         fmt.Printf("DNS Resolutions:\n")
         for _, resolve := range ip_data.Resolutions {
-            fmt.Printf("\t%s -> %s\n", resolve["last_resolved"], resolve["domain"])
+            fmt.Printf("\t%s -> %s\n", resolve.LastResolved, resolve.Domain)
         }
         fmt.Printf("References:\n")
         for _, reference := range ip_data.References {
@@ -64,7 +64,7 @@ func main(){
         }
         fmt.Printf("Resolutions:\n")
         for _, resolve := range domain_data.Resolutions {
-            fmt.Printf("\t%s -> %s\n", resolve["last_resolved"], resolve["ip_address"])
+            fmt.Printf("\t%s -> %s\n", resolve.LastResolved, resolve.IPAddress)
         }
 
     } else if *report == "email" {
@@ -97,7 +97,31 @@ func main(){
         for _, hash := range av_data.Hashes {
             fmt.Printf("\t%s\n", hash)
         }
+    } else if *report == "file" {
+        fmt.Printf("Looking up Hash: %s\n", *target)
+        file_data, err := gothreat.FileReport(*target)
+        if err != nil {
+            panic(err)
+        }
+        fmt.Printf("Permalink: %s\n", file_data.Permalink)
+        fmt.Printf("References:\n")
+        for _, reference := range file_data.References {
+            fmt.Printf("\t%s\n", reference)
+        }
+        fmt.Printf("MD5: %s\n", file_data.Md5)
+        fmt.Printf("SHA1: %s\n", file_data.Sha1)
+        fmt.Printf("Scans:\n")
+        for _, scan := range file_data.Scans {
+            fmt.Printf("\t%s\n", scan)
+        }
+        fmt.Printf("IPs:\n")
+        for _, ip := range file_data.Ips {
+            fmt.Printf("\t%s\n", ip)
+        }
+        fmt.Printf("Domain:\n")
+        for _, domain := range file_data.Domains {
+            fmt.Printf("\t%s\n", domain)
+        }
     }
-
 
 }
